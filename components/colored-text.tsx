f48bad7c467ce0c248a1099263c86d049bd0c6b8
0xstim <thang.victor.luong@gmail.com>
2023-10-18 12:40:53 +0700

    new section

--- components/colored-text.tsx
+++ components/colored-text.tsx
@@ -1,11 +1,20 @@
 interface ColoredTextProps {
   text: string;
   color: string;
+  code?: boolean;
 }
 
 export default function ColoredText(props: ColoredTextProps) {
-  const { text, color } = props;
+  const { text, color, code } = props;
   
+  if (code) {
+    return (
+      <>
+        <code className="nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10"><span style={{ color: color }}>{text}</span></code>
+      </>
+    )
+  }
+
   return (
     <>
       <span style={{ color: color }}>{text}</span>
